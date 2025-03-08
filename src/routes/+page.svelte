9bb694ea99dd6c46922e63b306e704da056284e6
PD <89239979+PDgaming@users.noreply.github.com>
2025-03-08 20:05:26 +0530

    added speak feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,7 @@
 	} from '@google/generative-ai';
 	import Loading from './components/loading.svelte';
 	import { writable } from 'svelte/store';
+	import Sidebar from './components/sidebar.svelte';
 
 	let messages: messagesType[] = [];
 	let conversations: ConversationType[] = [];
@@ -172,7 +173,7 @@
 		if (userInput == '' || null) {
 			const errorMessage: MessageType = {
 				content: "Sorry, you didn't enter a prompt.",
-				sender: `Gemini-${modal}`,
+				sender: $selectedModal,
 				time: new Date().toLocaleTimeString()
 			};
 			messages = [...messages, errorMessage];
@@ -204,7 +205,7 @@
 
 				const aiMessage: MessageType = {
 					content: text,
-					sender: `Gemini-${modal}`,
+					sender: $selectedModal,
 					time: new Date().toLocaleTimeString()
 				};
 
@@ -238,6 +239,19 @@
 		selectedModal.set(modal.id);
 		localStorage.setItem('SelectedModal', modal.id);
 	}
+	function speak(messageContent: string) {
+		const formattedContent = messageContent
+			.replace(/<p>/g, '')
+			.replace(/<\/p>/g, '')
+			.replace(/<b>/g, '')
+			.replace(/<\/b>/g, '');
+		const utterance = new SpeechSynthesisUtterance(formattedContent);
+
+		const voices = speechSynthesis.getVoices();
+		utterance.voice = voices[0]; // Use the first voice
+
+		speechSynthesis.speak(utterance);
+	}
 </script>
 
 <svelte:head>
@@ -245,14 +259,16 @@
 </svelte:head>
 
 <div class="main flex">
-	<div class="side-bar"></div>
+	<div class="side-bar" id="side-bar">
+		<Sidebar />
+	</div>
 	<div class="content">
 		{#if $welcomeMessage}
 			<div class="welcome-message flex h-screen w-screen items-center justify-center">
 				<p class="text-center text-3xl">How can i help you today?</p>
 			</div>
 		{/if}
-		<div class="chat-log hidden" id="chat-log">
+		<div class="chat-log hidden w-screen max-w-5xl" id="chat-log">
 			{#each messages as message}
 				{#if message.sender == 'User'}
 					<div class="user">
@@ -267,6 +283,37 @@
 							<div class="chat-header">{message.sender}</div>
 							<div class="chat-bubble bg-base-200">
 								{@html message.content}
+								<button
+									aria-label="Speak"
+									on:click={() => {
+										speak(message.content);
+									}}
+									class="tooltip btn btn-ghost"
+									data-tip="Speak"
+								>
+									<svg
+										width="20px"
+										height="20px"
+										viewBox="0 0 24 24"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											fill-rule="evenodd"
+											clip-rule="evenodd"
+											d="M10.4 1.8C11.5532 0.262376 14 1.07799 14 3.00001V21.1214C14 23.0539 11.5313 23.8627 10.3878 22.3049L6.49356 17H4C2.34315 17 1 15.6569 1 14V10C1 8.34315 2.34315 7 4 7H6.5L10.4 1.8ZM12 3L8.1 8.2C7.72229 8.70361 7.12951 9 6.5 9H4C3.44772 9 3 9.44772 3 10V14C3 14.5523 3.44772 15 4 15H6.49356C7.13031 15 7.72901 15.3032 8.10581 15.8165L12 21.1214V3Z"
+											fill="#6B7280"
+										/>
+										<path
+											d="M16.2137 4.17445C16.1094 3.56451 16.5773 3 17.1961 3C17.6635 3 18.0648 3.328 18.1464 3.78824C18.4242 5.35347 19 8.96465 19 12C19 15.0353 18.4242 18.6465 18.1464 20.2118C18.0648 20.672 17.6635 21 17.1961 21C16.5773 21 16.1094 20.4355 16.2137 19.8256C16.5074 18.1073 17 14.8074 17 12C17 9.19264 16.5074 5.8927 16.2137 4.17445Z"
+											fill="#6B7280"
+										/>
+										<path
+											d="M21.41 5C20.7346 5 20.2402 5.69397 20.3966 6.35098C20.6758 7.52413 21 9.4379 21 12C21 14.5621 20.6758 16.4759 20.3966 17.649C20.2402 18.306 20.7346 19 21.41 19C21.7716 19 22.0974 18.7944 22.2101 18.4509C22.5034 17.5569 23 15.5233 23 12C23 8.47672 22.5034 6.44306 22.2101 5.54913C22.0974 5.20556 21.7716 5 21.41 5Z"
+											fill="#6B7280"
+										/>
+									</svg>
+								</button>
 							</div>
 						</div>
 					</div>
@@ -483,6 +530,12 @@
 		height: calc(100vh - 150px);
 		width: 100vw;
 		overflow-y: scroll;
+		justify-content: flex-start;
+		position: absolute;
+		left: 50%;
+		transform: translate(-50%, 0%);
+		gap: 5px;
+		align-items: center;
 	}
 	.welcome-message {
 		margin-top: -100px;
---
+++ src/routes/components/sidebar.svelte
@@ -0,0 +1,15 @@
+<div class="drawer">
+	<input id="my-drawer" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<!-- Page content here -->
+		<label for="my-drawer" class="drawer-button btn btn-primary">Open drawer</label>
+	</div>
+	<div class="drawer-side">
+		<label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
+			<!-- Sidebar content here -->
+			<li>Sidebar Item 1</li>
+			<li>Sidebar Item 2</li>
+		</ul>
+	</div>
+</div>
