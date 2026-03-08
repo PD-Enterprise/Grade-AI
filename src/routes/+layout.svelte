ee8a40a5f1b7190dab952697a25d337762a6c9ba
PDgaming <produnyadehingia12345@gmail.com>
2026-03-08 16:04:55 +0530

    starting from scratch - sidebar is somewhat done.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Sidebar from './components/sidebar.svelte';
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<div class="main flex overflow-y-hidden">
+	<section class="sidebar flex-1 bg-base-200 h-screen p-2">
+		<Sidebar/>
+	</section>
+	<section class="body flex-5 h-screen p-2">
+		{@render children()}
+	</section>
+</div>
---
+++ src/routes/components/sidebar.svelte
@@ -0,0 +1,13 @@
+<div class="sidebar flex h-full flex-col gap-2">
+	<header class="header self-center">
+		<a class="btn rounded text-xl font-bold text-white btn-ghost btn-accent" href="/">Grade AI</a>
+	</header>
+	<div class="list flex-1 overflow-y-scroll">
+		{#each Array(100) as _, i}
+			<span>This is a thread name</span>
+		{/each}
+	</div>
+	<div class="auth flex flex-col justify-end">
+		<button class="btn rounded btn-accent">Login</button>
+	</div>
+</div>
