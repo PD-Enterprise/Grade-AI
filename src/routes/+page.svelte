24fd3ffc98678bce148ee1b323ff4a34949487a2
PDgaming <produnyadehingia12345@gmail.com>
2026-03-08 16:32:15 +0530

    minor changes to sidebar, and new chat page complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,28 @@
-New Chat
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="new-chat flex h-full flex-col items-center justify-center gap-5">
+	<div class="welcome-title">
+		<h1 class="text-4xl font-bold">Welcome to Grade AI</h1>
+	</div>
+	<div class="input-group flex w-[60%] flex-col gap-3 rounded-2xl bg-base-200 p-3">
+		<div class="input-field">
+			<input
+				type="text"
+				placeholder="Enter your question here"
+				class="input w-full rounded border-none bg-base-200 p-0 focus:outline-none"
+			/>
+		</div>
+		<div class="action-bar flex flex-row justify-between gap-3">
+			<select class="model-selection select rounded border-none bg-base-200 focus:outline-none">
+				{#each Array(3) as _, i}
+					<option value={i}>Model {i + 1}</option>
+				{/each}
+			</select>
+			<button class="btn rounded hover:bg-base-100"
+				><Icon icon="ic:round-send" width="24" height="24" /></button
+			>
+		</div>
+	</div>
+</div>
--- src/routes/components/sidebar.svelte
+++ src/routes/components/sidebar.svelte
@@ -2,12 +2,14 @@
 	<header class="header self-center">
 		<a class="btn rounded text-xl font-bold text-white btn-ghost btn-accent" href="/">Grade AI</a>
 	</header>
-	<div class="list flex-1 overflow-y-scroll">
-		{#each Array(100) as _, i}
-			<span>This is a thread name</span>
+	<div class="list flex flex-1 flex-col gap-1 overflow-y-scroll">
+		{#each Array(20) as _, i}
+			<a href={`/chat/${i + 1}`} class="btn h-7 justify-start rounded btn-ghost hover:bg-base-100"
+				>This is thread {i + 1}</a
+			>
 		{/each}
 	</div>
 	<div class="auth flex flex-col justify-end">
-		<button class="btn rounded btn-accent">Login</button>
+		<button class="btn rounded bg-base-300 hover:bg-base-100">Login</button>
 	</div>
 </div>
