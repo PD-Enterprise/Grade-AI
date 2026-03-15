39bbcedec0097e0c1bc194848647ce82d478519d
PDgaming <produnyadehingia12345@gmail.com>
2026-03-15 22:01:17 +0530

    adding new user to db and fetching user role

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,31 @@
 import type { UserData } from '$lib/types';
+import config from '$lib/utils/apiConfig';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const { user, session } = locals;
+	let membership: UserData['membership'] | undefined;
 
-	const membership: UserData['membership'] | undefined = 'tier-1';
+	if (session && user) {
+		// Add new user to Database
+		const userInsertion = await insertUser(user.email, user.name, user.image);
+		if (userInsertion instanceof Error) {
+			return {
+				status: 500,
+				message: 'Error inserting user'
+			};
+		}
+
+		// Get user role
+		const userRole = await getUserRole(user.email);
+		if (userRole instanceof Error) {
+			return {
+				status: 500,
+				message: 'Error getting user role'
+			};
+		}
+		membership = userRole;
+	}
 
 	return {
 		session,
@@ -12,3 +33,39 @@ export const load: LayoutServerLoad = async ({ locals }) => {
 		membership
 	};
 };
+
+async function insertUser(email: string, name: string, image: string | null) {
+	try {
+		const request = await fetch(`${config.apiUrl}users/new-user`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				email: email,
+				name: name,
+				avatarUrl: image
+			})
+		});
+
+		return await request.json();
+	} catch (error) {
+		return new Error('Error inserting user');
+	}
+}
+
+async function getUserRole(email: string) {
+	try {
+		const request = await fetch(`${config.apiUrl}users/roles/get-role`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				email: email
+			})
+		});
+		const result = await request.json();
+		// console.log(result.data);
+
+		return result.data;
+	} catch (error) {
+		return new Error('Error getting user role');
+	}
+}
