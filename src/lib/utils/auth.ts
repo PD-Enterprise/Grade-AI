3bc155c9bce99dfef84c6a2630f418c0c5ccb309
PDgaming <produnyadehingia12345@gmail.com>
2026-05-17 21:03:20 +0530

    minor change: manually sending the env secret to the client creation

--- src/lib/utils/auth.ts
+++ src/lib/utils/auth.ts
@@ -1,9 +1,15 @@
 import { getRequestEvent } from '$app/server';
-import { BETTER_AUTH_URL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import {
+	BETTER_AUTH_SECRET,
+	BETTER_AUTH_URL,
+	GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET
+} from '$env/static/private';
 import { betterAuth } from 'better-auth';
 import { sveltekitCookies } from 'better-auth/svelte-kit';
 
 export const auth = betterAuth({
+	secret: BETTER_AUTH_SECRET,
 	baseURL: BETTER_AUTH_URL,
 	socialProviders: {
 		google: {
